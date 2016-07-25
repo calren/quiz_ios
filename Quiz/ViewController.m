9b0e77daa18855eb642490393a386a87a35484f7
calren <calren24@gmail.com>
2016-07-24 23:06:48 -0700

    Adding array of questions and answers

--- Quiz/ViewController.m
+++ Quiz/ViewController.m
@@ -10,18 +10,39 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *questionLabel;
+@property (weak, nonatomic) IBOutlet UILabel *answerLabel;
+@property (weak, nonatomic) IBOutlet UIButton *questionButton;
+@property (weak, nonatomic) IBOutlet UIButton *answerButton;
+
+@property (nonatomic) int currentQuestionIndex;
+
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+- (IBAction)showQuestion:(id)sender {
+    int lowerBound = 0;
+    int upperBound = 3;
+    self.currentQuestionIndex = lowerBound + arc4random() % (upperBound - lowerBound);
+    
+    NSArray *questions = @[@"How many Pokemons are in the first generation?", @"What is Ash's first Pokemon?", @"What are the three starter Pokemons?"];
+
+    
+//    NSString *testString = @"What kind of Pokemon is Squirtle?";
+//    self.questionLabel.text = testString;
+    
+    NSString *currentQuestion = questions[self.currentQuestionIndex];
+    self.questionLabel.text = currentQuestion;
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)showAnswer:(id)sender {
+//    NSString *testString = @"Water";
+//    self.answerLabel.text = testString;
+    NSArray *answers = @[@"150", @"Pikachu", @"Bulbasaur, Charmander, Squirtle"];
+    self.answerLabel.text = answers[self.currentQuestionIndex];
 }
+
 
 @end
